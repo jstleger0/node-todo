8c2204e072a347bf8d6da906e664e907162fe884
John St Leger <john.stleger@havashelia.com>
2016-03-21 17:05:11 +0000

    Changed the DB URL to use EC2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
 
 	// the database url to connect!
-	url: 'mongodb://todoAholic:scotchio@ec2-52-0-14-185.compute-1.amazonaws.com:27017/dummyDB'
+	// url: 'mongodb://todoAholic:scotchio@ec2-52-0-14-185.compute-1.amazonaws.com:27017/dummyDB'
+	url: 'mongodb://jstleger0:Matthewtc1@ec2-52-50-181-208.eu-west-1.compute.amazonaws.com:27017/dummyDB'
 }
